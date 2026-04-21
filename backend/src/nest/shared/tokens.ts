d200f3f6c8c57b0141e9b447b2c39695ca7af7cc
Luiz Paulo <luiz.lira029@gmail.com>
2026-04-21 10:30:37 -0300

    feat: create decorator to roles

--- backend/src/nest/shared/tokens.ts
+++ backend/src/nest/shared/tokens.ts
@@ -2,8 +2,8 @@ export const TOKENS = {
   usersRepo: Symbol('usersRepo'),
   categoriesRepo: Symbol('categoriesRepo'),
   productsRepo: Symbol('productsRepo'),
+  favoritesRepo: Symbol('favoritesRepo'),
   authCredentialsRepo: Symbol('authCredentialsRepo'),
   idGenerator: Symbol('idGenerator'),
   clock: Symbol('clock'),
 } as const;
-
